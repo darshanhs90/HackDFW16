c5bcdd2210f456b345510060feffb149ddef3463
darshanhs90 <hsdars@gmail.com>
2016-04-16 22:12:24 -0500

    tropo end points added

---
+++ spark.js
@@ -0,0 +1,15 @@
+// make video call
+//https://api.tropo.com/1.0/sessions?action=create&token=436e6379527861734f776f444e4b5a4b43675378484959496b4442466f4e515167575678694a595248454862&actionPerform=1&sip=82921369@ciscospark.com
+
+
+//send sms
+//https://api.tropo.com/1.0/sessions?action=create&token=436e6379527861734f776f444e4b5a4b43675378484959496b4442466f4e515167575678694a595248454862&actionPerform=2&ph=14697672278
+
+
+//verify on login
+
+
+//call conference number
+//+1317-659-0432
+
+
