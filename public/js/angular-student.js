fb830a775c817cc778d22711d46f5253c8fbf67a
darshanhs90 <hsdars@gmail.com>
2016-04-17 11:36:52 -0500

    asdasd

--- public/js/angular-student.js
+++ public/js/angular-student.js
@@ -164,3 +164,14 @@ $scope.completeVerify=function(){
 }
 
 });
+
+app.directive('backImg', function(){
+    return function(scope, element, attrs){
+        attrs.$observe('backImg', function(value) {
+            element.css({
+                'background-image': 'url(' + value +')',
+                'background-size' : 'cover'
+            });
+        });
+    };
+});
